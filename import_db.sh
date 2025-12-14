fb4e3075712126883c55b698dd4077b2609333d5
Magdalena R <149877205+magdlenar@users.noreply.github.com>
2025-12-14 23:28:47 +0700

    Update import_db.sh

--- import_db.sh
+++ import_db.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -e
 
+echo "MYSQLHOST=$MYSQLHOST"
+echo "MYSQLPORT=$MYSQLPORT"
+echo "MYSQLUSER=$MYSQLUSER"
+echo "MYSQLDATABASE=$MYSQLDATABASE"
+echo "MYSQLPASSWORD is set? -> $( [ -n "$MYSQLPASSWORD" ] && echo YES || echo NO )"
+
 echo "Waiting for MySQL..."
-until mysql -h "$MYSQLHOST" -P "$MYSQLPORT" -u "$MYSQLUSER" -p"$MYSQLPASSWORD" -e "SELECT 1" "$MYSQLDATABASE"; do
+until mysql -h "$MYSQLHOST" -P "$MYSQLPORT" -u "$MYSQLUSER" -p"$MYSQLPASSWORD" -e "SELECT 1" "$MYSQLDATABASE" >/dev/null 2>&1; do
   sleep 3
 done
 
-echo "Importing database..."
+echo "Importing..."
 mysql -h "$MYSQLHOST" -P "$MYSQLPORT" -u "$MYSQLUSER" -p"$MYSQLPASSWORD" "$MYSQLDATABASE" < aplikasi_lab.sql
 
-echo "Import done."
+echo "Done."
