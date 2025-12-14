795ee2177385c0e2c65a8bf3838f822798aa87f4
Magdalena R <149877205+magdlenar@users.noreply.github.com>
2025-12-14 23:24:44 +0700

    Update import_db.sh

--- import_db.sh
+++ import_db.sh
@@ -2,11 +2,11 @@
 set -e
 
 echo "Waiting for MySQL..."
-until mysqladmin ping -h"$MYSQLHOST" -P"$MYSQLPORT" -u"$MYSQLUSER" -p"$MYSQLPASSWORD" --silent; do
-  sleep 2
+until mysql -h "$MYSQLHOST" -P "$MYSQLPORT" -u "$MYSQLUSER" -p"$MYSQLPASSWORD" -e "SELECT 1" "$MYSQLDATABASE"; do
+  sleep 3
 done
 
-echo "Importing aplikasi_lab.sql..."
+echo "Importing database..."
 mysql -h "$MYSQLHOST" -P "$MYSQLPORT" -u "$MYSQLUSER" -p"$MYSQLPASSWORD" "$MYSQLDATABASE" < aplikasi_lab.sql
 
-echo "Done."
+echo "Import done."
