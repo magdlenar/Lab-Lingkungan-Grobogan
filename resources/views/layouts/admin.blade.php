012326f2b30ab562142a0fc7f5e51bfda6e179e5
Magdalena R <149877205+magdlenar@users.noreply.github.com>
2025-12-14 18:45:25 +0700

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Admin Panel')</title>
-    <link rel="icon" type="image/jpg" href="{{ asset('images/logo.jpg') }}">
+    <link rel="icon" type="image/jpg" href="{{ secure_asset('images/logo.jpg') }}">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
     <!-- Bootstrap -->
