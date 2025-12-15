f5c72db664f6067219f710fa5fae7712ea42880f
Magdalena R <149877205+magdlenar@users.noreply.github.com>
2025-12-15 20:28:31 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Galeri;
-
+use App\Models\LabDocument;
 class HomeController extends Controller
 {
     public function index()
@@ -13,4 +13,9 @@ class HomeController extends Controller
 
         return view('home', compact('galeris'));
     }
+    public function layanan()
+{
+    $labDoc = LabDocument::first();
+    return view('layanan', compact('labDoc'));
+}
 }
