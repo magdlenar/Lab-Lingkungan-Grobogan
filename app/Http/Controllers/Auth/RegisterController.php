57cda18dbbabd1fcd0a4325705a9a3bb33bbb727
Magdalena R <149877205+magdlenar@users.noreply.github.com>
2025-12-17 22:25:38 +0700

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -41,14 +41,18 @@ class RegisterController extends Controller
             'role'              => 'customer', // default role
         ]);
 
-       try {
-            Mail::to($user->email)->send(new VerificationCodeMail($user));
-        } catch (\Exception $e) {
-            return back()->withErrors([
-                'email' => 'Gagal mengirim email verifikasi. Coba lagi nanti.'
-            ]);
-        }
+      \Log::info('Mulai kirim email verifikasi ke: ' . $user->email);
 
+    try {
+        Mail::to($user->email)->send(new VerificationCodeMail($user));
+        \Log::info('Email verification send() berhasil dipanggil');
+    } catch (\Throwable $e) {
+        \Log::error('Email verification failed: ' . $e->getMessage());
+    
+        return back()->withErrors([
+            'email' => 'Gagal mengirim email verifikasi. Coba lagi nanti.'
+        ]);
+    }
         // Simpan session untuk halaman verifikasi
         session(['verify_email' => $user->email]);
 
